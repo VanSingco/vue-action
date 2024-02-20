226c754e40e7cce91a9df721fee6e1e01f2cd3a2
Van Zachary Singco <vanske19@gmail.com>
2024-02-20 14:37:55 +0800

    Change text 5

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <h1>Hello World Vanske 123</h1>
+      <h1>Hello World Vanske</h1>
     </div>
   </header>
 
